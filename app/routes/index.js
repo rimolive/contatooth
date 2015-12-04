aad483d72b88477428d5eaaf00f44509ae27b526
Ricardo M. Oliveira <rmartine@rmartine.gru.fedora>
2015-12-04 17:31:23 -0200

    Testing

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,6 +3,7 @@ module.exports = function(app) {
 		//var login = '';
 		//if(req.user) {
 		//	login = req.user.login;
+			login = "Testing";
 		//}
 		res.render('index', {"usuarioLogado": login});
 	});
