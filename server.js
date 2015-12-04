898538c1cb5df9baae9159271177727a87e55682
Ricardo M. Oliveira <rmartine@rmartine.gru.fedora>
2015-12-04 17:39:01 -0200

    Adding user/pass

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var http = require('http');
 var express = require('express');
-var app = require('./config/express')(app);
+v
+r app = require('./config/express')(app);
 require('./config/passport')();
-require('./config/database.js')('mongodb://172.30.253.38/contatooth');
+require('./config/database.js')('mongodb://admin:RQayuslUdchmbOYX@172.30.253.38/contatooth');
 
 http.createServer(app).listen(app.get('port'), function() {
 	console.log('Express server escutando na porta ' + app.get('port'));
