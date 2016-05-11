033017ee850e1ab54486d2c55908cf5780be9100
Ricardo Martinelli de Oliveira <ricardo.martinelli.oliveira@gmail.com>
2016-05-11 06:50:08 -0300

    Update consulta.js

--- consulta.js
+++ consulta.js
@@ -3,7 +3,7 @@ var ObjectID = require('mongodb').ObjectID;
 
 var _idProcurado = new ObjectID('5652017292afb1f05f0d0b3a');
 
-MongoClient.connect('mongodb://127.0.0.1:27017/contatooth',
+MongoClient.connect('mongodb://user8UY:StsiCQ8jpUpBF3b3@172.30.101.124:27017/sampledb',
 	function(erro, db) {
 		if(erro) throw erro;
 		db.collection('contatos').findOne({_id: _idProcurado},
