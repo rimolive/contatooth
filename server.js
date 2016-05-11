9f55fb513bc78c7d6363541bd40e7bbfbfea590c
Ricardo Martinelli de Oliveira <ricardo.martinelli.oliveira@gmail.com>
2016-05-11 06:56:41 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var express = require('express');
 var app = require('./config/express')(app);
 require('./config/passport')();
-require('./config/database.js')('mongodb://localhost/contatooth', 'user8UY', 'StsiCQ8jpUpBF3b3');
+require('./config/database.js')('mongodb://user8UY:StsiCQ8jpUpBF3b3@172.30.101.124/sampledb');
 
 http.createServer(app).listen(app.get('port'), function() {
 	console.log('Express server escutando na porta ' + app.get('port'));
